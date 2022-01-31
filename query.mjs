33110aaab6861da2c8d9292a3e613948807d9f7b
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-01-31 16:25:32 +0100

    added async filter with filtering based on presence of a CITATION.cff in the root of the repo

--- query.mjs
+++ query.mjs
@@ -2,57 +2,52 @@ import { Octokit } from "octokit";
 import * as fs from 'fs';
 
 
-const include_whitelisted = (url) => {
-    const whitelist = [
-        'https://github.com/3D-e-Chem/tycho-knime-node-archetype',
-        'https://github.com/AA-ALERT/frbcatdb',
-        'https://github.com/abrupt-climate/hyper-canny',
-        'https://github.com/ADAH-EviDENce/evidence',
-        'https://github.com/amusecode/amuse',
-        'https://github.com/arabic-digital-humanities/adhtools',
-        'https://github.com/arnikz/PIQMIe',
-        'https://github.com/benvanwerkhoven/kernel_tuner',
-        'https://github.com/c-martinez/BeyondTheBook',
-        'https://github.com/c3s-magic/c3s-magic-wps',
-        'https://github.com/candYgene/siga',
-        'https://github.com/caselawanalytics/CaseLawAnalytics',
-        'https://github.com/ci-for-science/self-hosted-runners',
-        'https://github.com/citation-file-format/citation-file-format',
-        'https://github.com/CLARIAH/grlc',
-        'https://github.com/DeepRank/pssmgen',
-        'https://github.com/dianna-ai/dianna',
-        'https://github.com/DynaSlum/satsense',
-        'https://github.com/e-mental-health/data-processing'
-    ];
+const loadFromJsonfile = (filename) => {
+    const data = fs.readFileSync(filename, 'utf8');
+    return JSON.parse(data);
+}
+
+
+const includeWhitelisted = (url) => {
     return whitelist.includes(url);
 }
 
-const include_with_cff = async (url) => {
 
-    const has_citationcff = (treeitem) => {
-        return treeitem.path == 'CITATION.cff';
-    };
+const includeHasCitationcff = async (url) => {
 
     const [ owner, repo, ...unuseds ] = url.slice("https://github.com/".length).split('/');
 
-    // const { data: { name } } = await octokit.rest.repos.
-    // console.log(name);
+    const { data: { default_branch } } = await octokit.request('GET /repos/{owner}/{repo}', { owner, repo });
 
     const { data: { tree } } = await octokit.rest.git.getTree({
         owner,
         repo,
-        tree_sha: 'master'
+        tree_sha: default_branch
     });
-    return tree.filter(has_citationcff).length == 1;
+    return tree.filter(treeitem => treeitem.path == 'CITATION.cff').length == 1;
 }
 
-const data = fs.readFileSync('./urls.json', 'utf8');
-const urls_all = JSON.parse(data);
+
+const filterAsync = async (arr, asyncCallback) => {
+    const promises = arr.map(asyncCallback);
+    const results = await Promise.all(promises);
+    return results.map((result, index) => {
+        return {
+            result: result,
+            value: arr[index]
+        }
+    })
+    .filter(item => item.result === true)
+    .map(item => item.value);
+}
+
+
+const urls_rsd = loadFromJsonfile('./urls.json');
+const whitelist = loadFromJsonfile('./whitelist.json');
 
 const octokit = new Octokit({auth: process.env.GITHUB_TOKEN});
 
-const urls = await urls_all.filter(include_whitelisted)
-                           .filter(include_with_cff);
+const urls = await filterAsync(whitelist, includeHasCitationcff);
 
 console.log(urls);
 
