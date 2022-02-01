3e1cb08fe8daa45b040105a2a7d968335a094981
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-02-01 11:53:03 +0100

    added filter for checking the repo is using workflows; refs #5

--- query.mjs
+++ query.mjs
@@ -16,15 +16,24 @@ const includeWhitelisted = (url) => {
 const includeHasCitationcff = async (url) => {
 
     const [ owner, repo, ...unuseds ] = url.slice("https://github.com/".length).split('/');
-
     const { data: { default_branch } } = await octokit.request('GET /repos/{owner}/{repo}', { owner, repo });
-
     const { data: { tree } } = await octokit.rest.git.getTree({
         owner,
         repo,
         tree_sha: default_branch
     });
     return tree.filter(treeitem => treeitem.path == 'CITATION.cff').length == 1;
+}
+
+
+const includeUsesWorkflows = async (url) => {
+
+    const [ owner, repo, ...unuseds ] = url.slice("https://github.com/".length).split('/');
+    const { data: { total_count: nworkflows } } = await octokit.rest.actions.listRepoWorkflows({
+        owner,
+        repo,
+    });
+    return nworkflows > nworkflows_minimum;
 }
 
 
@@ -45,11 +54,13 @@ const filterAsync = async (arr, asyncCallback) => {
 const urls_rsd = loadFromJsonfile('./urls.json');
 const whitelist = loadFromJsonfile('./whitelist.json');
 
+const nworkflows_minimum = 1;
+
 const octokit = new Octokit({auth: process.env.GITHUB_TOKEN});
 
 let urls = urls_rsd;
-urls = urls.filter(includeWhitelisted);
-urls = await filterAsync(whitelist, includeHasCitationcff);
+urls = await filterAsync(urls, includeHasCitationcff);
+urls = await filterAsync(urls, includeUsesWorkflows);
 
 console.log(urls);
 
